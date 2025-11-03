cdc611e5b994b21a2b36f7b4073729a5a2789bc6
Nexxus314 <nexxusromsbeta@gmail.com>
2025-11-03 13:29:26 +0530

    made changes to delete button

--- src/components/TaskInput.jsx
+++ src/components/TaskInput.jsx
@@ -1,30 +1,51 @@
 import React from "react";
 import Cards from "./Cards";
+import { IoMdTrash } from "react-icons/io";
 
 const TaskInput = ({tasks,toggleTaskCompletion,deleteTask}) => {
+
+ if(!tasks || tasks.length==0){
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <p className="text-gray-500 text-xl font-medium">No Tasks Available Yet.</p>
+    </div>
+  )
+ };
+  
   return (
     <>
+    
     <div className="flex flex-row">
       {tasks.map((task)=>(
 
-         <Cards key={task.id}>
+         <Cards key={task.id} className="group">
+
       <div className="flex flex-col text-center items-center gap-3 p-3">
-        <h3 className={`text-lg font-bold text-gray-800 ${
-                task.completed ? "line-through text-gray-400" : "text-gray-800"
+        <h3 className={`text-3xl font-bold ${
+                task.completed ? "line-through text-gray-400" : "text-indigo-800"
               }`}>{task.taskname}</h3>
-        <p className={` text-xs font-medium uppercase text-gray-500 ${ task.completed ? "line-through text-gray-400" : "text-gray-500"
+        <p className={` text-xl font-medium text-gray-500 ${ task.completed ? "line-through text-gray-400" : "text-gray-500"
               }`}>
            {task.taskinfo}
         </p>
-        <input type="checkbox" className="w-6 h-6"
-        checked={task.completed}
-              onChange={() => toggleTaskCompletion(task.id)} />
-        <button onClick={()=>deleteTask(task.id)} className="text-sm font-bold mt-3 bg-indigo-400 p-1 rounded-lg hover:bg-indigo-600">Delete Task.</button>
 
-      </div>
-
-
+            <input
+              type="checkbox"
+              className="w-6 h-6 mt-3 mb-0 accent-indigo-500 cursor-pointer"
+              checked={task.completed}
+              onChange={() => toggleTaskCompletion(task.id)}
+            />
+            <button
+              onClick={() => deleteTask(task.id)}
+              className="mt-5"
+            >
+             <IoMdTrash size={30} />
+            </button>
+          </div>
+          
     </Cards>
+    
+    
       ))}
     </div>
     
--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -14,14 +14,14 @@ const TaskManager = () => {
     console.error("Error parsing localStorage data:", error);
     savedTasks = [];
   }
-  const [tasks, setTasks] = useState([savedTasks]);
+  const [tasks, setTasks] = useState(savedTasks);
   const [showNewTask, setShowNewTask] = useState(false);
   //used to hide the newtask form so that it doesnt stay there
   const [filter, setFilter] = useState("all");
 
   const filteredTasks = tasks.filter((task) => {
-    if (filter == "active") return task.completed;
-    if (filter == "completed") return !task.completed;
+    if (filter == "active") return !task.completed;
+    if (filter == "completed") return task.completed;
     return true;
   });
 
