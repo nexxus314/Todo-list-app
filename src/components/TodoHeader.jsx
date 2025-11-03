9c5abe3dea462fef4f9ba241f09c3e13f3a3fd8b
Nexxus314 <nexxusromsbeta@gmail.com>
2025-11-03 16:13:29 +0530

    responsive for iphone

--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -6,7 +6,7 @@ const TodoHeader = ({setFilter,onNewTaskClick,currentFilter}) => {
     
 
 <nav className="bg-indigo-400 border-gray-200 dark:bg-indigo-400">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
 
         <span className="self-center text-2xl font-mono font-semibold whitespace-nowrap dark:text-white ">
           ToDo.</span>
